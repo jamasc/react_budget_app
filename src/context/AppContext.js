0b7a35d79eeaae582d9a44ac4a8809047d3d8dae
jamasc <schittenhelmm@queens.edu>
2023-09-27 18:01:58 -0400

    lab done

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -30,46 +30,46 @@ export const AppReducer = (state, action) => {
                     ...state
                 }
             }
-            case 'RED_EXPENSE':
-                const red_expenses = state.expenses.map((currentExp)=> {
-                    if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
-                        currentExp.cost =  currentExp.cost - action.payload.cost;
-                        budget = state.budget + action.payload.cost
-                    }
-                    return currentExp
-                })
-                action.type = "DONE";
-                return {
-                    ...state,
-                    expenses: [...red_expenses],
-                };
-            case 'DELETE_EXPENSE':
-            action.type = "DONE";
-            state.expenses.map((currentExp)=> {
-                if (currentExp.name === action.payload) {
-                    budget = state.budget + currentExp.cost
-                    currentExp.cost =  0;
+        case 'RED_EXPENSE':
+            const red_expenses = state.expenses.map((currentExp)=> {
+                if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
+                    currentExp.cost =  currentExp.cost - action.payload.cost;
+                    budget = state.budget + action.payload.cost
                 }
                 return currentExp
             })
             action.type = "DONE";
             return {
                 ...state,
-                budget
+                expenses: [...red_expenses],
             };
-        case 'SET_BUDGET':
-            action.type = "DONE";
-            state.budget = action.payload;
-
-            return {
-                ...state,
-            };
-        case 'CHG_CURRENCY':
-            action.type = "DONE";
-            state.currency = action.payload;
-            return {
-                ...state
+        case 'DELETE_EXPENSE':
+        action.type = "DONE";
+        state.expenses.map((currentExp)=> {
+            if (currentExp.name === action.payload) {
+                budget = state.budget + currentExp.cost
+                currentExp.cost =  0;
             }
+            return currentExp
+        })
+        action.type = "DONE";
+        return {
+            ...state,
+            budget
+        };
+    case 'SET_BUDGET':
+        action.type = "DONE";
+        state.budget = action.payload;
+
+        return {
+            ...state,
+        };
+    case 'CHG_CURRENCY':
+        action.type = "DONE";
+        state.currency = action.payload;
+        return {
+            ...state
+        }
 
         default:
             return state;
@@ -78,15 +78,15 @@ export const AppReducer = (state, action) => {
 
 // 1. Sets the initial state when the app loads
 const initialState = {
-    budget: 2000,
+    budget: 420,
     expenses: [
-        { id: "Marketing", name: 'Marketing', cost: 50 },
-        { id: "Finance", name: 'Finance', cost: 300 },
-        { id: "Sales", name: 'Sales', cost: 70 },
-        { id: "Human Resource", name: 'Human Resource', cost: 40 },
-        { id: "IT", name: 'IT', cost: 500 },
+        { id: "Marketing", name: 'Marketing', cost: 69 },
+        { id: "Finance", name: 'Finance', cost: 69 },
+        { id: "Sales", name: 'Sales', cost: 69 },
+        { id: "Human Resource", name: 'Human Resource', cost: 69 },
+        { id: "IT", name: 'IT', cost: 69 },
     ],
-    currency: '£'
+    currency: '€'
 };
 
 // 2. Creates the context this is the thing our components import and use to get the state
